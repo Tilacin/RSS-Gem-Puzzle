a1d3e47218c6ab572e213aea5c70966fc57ff0ba
Tilacin <tilacin45@gmail.com>
2022-10-22 16:01:25 +0300

    init: size 4x4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Заполняем body
-
+/*
 let div = document.createElement("div");
 const body = document.getElementsByTagName("body");
 div.classList.add("wrapper");
@@ -13,25 +13,71 @@ document.body.insertAdjacentHTML(
     <botton class="btn_save">Save</botton>
     <botton class="btn_results">Results</botton>
 </div>
-<div class="playing_field4">
-   <div class="number1 number"></div>
-   <div class="number2 number"></div>
-   <div class="number3 number"></div>
-   <div class="number4 number"></div>
-   <div class="number5 number"></div>
-   <div class="number6 number"></div>
-   <div class="number7 number"></div>
-   <div class="number8 number"></div>
+<div  id="elem" class="playing_field4">
+   <div class="number1 number number_size4"></div>
+   <div class="number2 number number_size4"></div>
+   <div class="number3 number number_size4"></div>
+   <div class="number4 number number_size4"></div>
+   <div class="number5 number number_size4"></div>
+   <div class="number6 number number_size4"></div>
+   <div class="number7 number number_size4"></div>
+   <div class="number8 number number_size4"></div>
 
-   <div class="number9 number"></div>
-   <div class="number10 number"></div>
-   <div class="number11 number"></div>
-   <div class="number12 number"></div>
-   <div class="number13 number"></div>
-   <div class="number14 number"></div>
-   <div class="number15 number"></div>
-  
-  
+   <div class="number9 number number_size4"></div>
+   <div class="number10 number number_size4"></div>
+   <div class="number11 number number_size4"></div>
+   <div class="number12 number number_size4"></div>
+   <div class="number13 number number_size4"></div>
+   <div class="number14 number number_size4"></div>
+   <div class="number15 number number_size4"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size5"></div>
+   <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size6"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size7"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
+    <div class="number number_size8"></div>
 </div>
 <div class="field_size">
     <div class="size size3">3x3</div>
@@ -46,7 +92,12 @@ document.body.insertAdjacentHTML(
 </div>`
 );
 //размер поля 4x4
-
+//Заполняем  числами из массива
+let size4 = document.querySelector(".size4");
+const start = document.querySelector(".btn_start");
+const stop = document.querySelector(".btn_stop");
+let number = document.querySelectorAll(".number");
+let playing = document.querySelector("#elem");
 //перемешиваем массив
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
@@ -62,9 +113,7 @@ function shuffle(arr) {
   return arr;
 }
 //Заполняем  числами из массива
-const start = document.querySelector(".btn_start");
-const stop = document.querySelector(".btn_stop");
-let number = document.querySelectorAll(".number");
+
 let arrShuffle = shuffle(arr);
 
 function startGame() {
@@ -79,23 +128,56 @@ start.addEventListener("click", startGame);
 stop.addEventListener("click", function () {
   alert("Эту кнопку, по заданию, реализовывать не нужно");
 });
+
 //--размер поля 5x5-------------------------------------------------------------------------------
-let playing4 = document.querySelector(".playing_field4");
-let playing5 = document.querySelector(".playing_field5");
+
 let size5 = document.querySelector(".size5");
+let number5 = document.querySelectorAll(".number_size5");
 size5.addEventListener("click", function () {
-  playing4.classList.toggle("playing_field5");
-  document.body.insertAdjacentHTML(
-    "beforeEnd",
-    `<div class="number15 number"></div>
-    <div class="number16 number"></div>
-    <div class="number17 number"></div>
-    <div class="number18 number"></div>
-    <div class="number19 number"></div>
-    <div class="number10 number"></div>
-    <div class="number21 number"></div>
-    <div class="number22 number"></div>
-    <div class="number23 number"></div>
-    <div class="number24 number"></div>`
-  );
+  number5.forEach((elem) => (elem.style.display = "block"));
+  elem.classList.add("playing_field5");
+  
+  let arr1 = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24,
+  ];
+  let arrShuffle = shuffle(arr1);
+  function startGame() {
+    number.forEach(function (div, i) {
+      div.innerHTML = arrShuffle[i];
+    })
+  shuffle(arr1)
+  }
+  startGame()
 });
+*/
+let div = document.createElement("div");
+div.className = "playing";
+document.body.prepend(div);
+const playing = document.querySelector(".playing");
+//координаты первой ячейки
+let coordinates = {
+  top: 0,
+  left: 0,
+};
+let size = 100;
+//создаём и заполняем цифрами ячейки
+for (let i = 1; i <= 15; i++) {
+  const number = document.createElement("div");
+  number.className = "number";
+  number.innerHTML = i;
+  //задаём позиции для элементов
+  let left = i % 4;
+  let top = (i - left) / 4;
+  number.style.left = `${left * size}px`;
+  number.style.top = `${top * size}px`;
+  playing.append(number);
+  //изменяем положение ячейки при клике
+  
+  number.addEventListener("click", () => {
+    number.style.left = `${coordinates.left * size}px`;
+    number.style.top = `${coordinates.top * size}px`;
+  });
+
+  
+}
