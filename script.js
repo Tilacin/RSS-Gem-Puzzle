6a88fdc8f96677f573b6760940af3eea2724992f
Tilacin <tilacin45@gmail.com>
2022-10-24 08:12:26 +0300

    feat:added fields of all sizes

--- script.js
+++ script.js
@@ -1,157 +1,4 @@
 //Заполняем body
-/*
-let div = document.createElement("div");
-const body = document.getElementsByTagName("body");
-div.classList.add("wrapper");
-const wrap = document.querySelector(".wrapper");
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  `<div class="wrapper">
-<div class="menu">
-    <botton class="btn_start">Shuffle and start</botton>
-    <botton class="btn_stop">Stop</botton>
-    <botton class="btn_save">Save</botton>
-    <botton class="btn_results">Results</botton>
-</div>
-<div  id="elem" class="playing_field4">
-   <div class="number1 number number_size4"></div>
-   <div class="number2 number number_size4"></div>
-   <div class="number3 number number_size4"></div>
-   <div class="number4 number number_size4"></div>
-   <div class="number5 number number_size4"></div>
-   <div class="number6 number number_size4"></div>
-   <div class="number7 number number_size4"></div>
-   <div class="number8 number number_size4"></div>
-
-   <div class="number9 number number_size4"></div>
-   <div class="number10 number number_size4"></div>
-   <div class="number11 number number_size4"></div>
-   <div class="number12 number number_size4"></div>
-   <div class="number13 number number_size4"></div>
-   <div class="number14 number number_size4"></div>
-   <div class="number15 number number_size4"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size5"></div>
-   <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size6"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size7"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-    <div class="number number_size8"></div>
-</div>
-<div class="field_size">
-    <div class="size size3">3x3</div>
-    <div class="size size4">4x4</div>
-    <div class="size size5">5x5</div>
-    <div class="size size6">6x6</div>
-    <div class="size size7">7x7</div>
-    <div class="size size8">8x8</div>
-</div>
-
-
-</div>`
-);
-//размер поля 4x4
-//Заполняем  числами из массива
-let size4 = document.querySelector(".size4");
-const start = document.querySelector(".btn_start");
-const stop = document.querySelector(".btn_stop");
-let number = document.querySelectorAll(".number");
-let playing = document.querySelector("#elem");
-//перемешиваем массив
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
-function shuffle(arr) {
-  var j, temp;
-  for (var i = arr.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = arr[j];
-    arr[j] = arr[i];
-    arr[i] = temp;
-  }
-  console.log(arr);
-  return arr;
-}
-//Заполняем  числами из массива
-
-let arrShuffle = shuffle(arr);
-
-function startGame() {
-  number.forEach(function (div, i) {
-    div.innerHTML = arrShuffle[i];
-  });
-  shuffle(arr); //каждый вызов перемешиваем массив
-}
-
-//вешаем обработчик на кнопку старт
-start.addEventListener("click", startGame);
-stop.addEventListener("click", function () {
-  alert("Эту кнопку, по заданию, реализовывать не нужно");
-});
-
-//--размер поля 5x5-------------------------------------------------------------------------------
-
-let size5 = document.querySelector(".size5");
-let number5 = document.querySelectorAll(".number_size5");
-size5.addEventListener("click", function () {
-  number5.forEach((elem) => (elem.style.display = "block"));
-  elem.classList.add("playing_field5");
-  
-  let arr1 = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24,
-  ];
-  let arrShuffle = shuffle(arr1);
-  function startGame() {
-    number.forEach(function (div, i) {
-      div.innerHTML = arrShuffle[i];
-    })
-  shuffle(arr1)
-  }
-  startGame()
-});
-*/
-//добавляем элементы на страницу
 
 let div = document.createElement("div");
 div.className = "playing";
@@ -274,12 +121,14 @@ for (let i = 1; i <= 15; i++) {
   });
 }
 
-////////////////////////////////////////////////////
+///////////////////  8x8  /////////////////////////////////
 var numbers = document.getElementsByClassName("number");
 function eightRows() {
   playing.innerHTML = "";
+  playing.style.width = "400px";
+  playing.style.height = "400px";
   size = 50;
-  flag = 8;
+  
 
   let coordinates = {
     top: 0,
@@ -327,7 +176,7 @@ function eightRows() {
   arrSort = arr1.sort(() => Math.random() - 0.5);
 
   console.log(arrSort);
-  //for (let i = 0; i < arrSort.length; i++) {
+
   for (let i = 1; i < 64; i++) {
     const number = document.createElement("div");
     let value = arrSort[i - 1];
@@ -361,9 +210,11 @@ function eightRows() {
 const size8 = document.querySelector(".size8");
 size8.addEventListener("click", eightRows);
 
-/////////////////////////////////////////////////////
+////////////////////////  4x4  /////////////////////////////
 function four() {
   playing.innerHTML = "";
+  playing.style.width = "400px";
+  playing.style.height = "400px";
   let size = 100;
   //координаты нулевой ячейки
   let coordinates = {
@@ -453,3 +304,350 @@ function four() {
 }
 const size4 = document.querySelector(".size4");
 size4.addEventListener("click", four);
+////////////////////////////  3x3  /////////////////////////////
+function threeRows() {
+  playing.innerHTML = "";
+  playing.style.width = "300px";
+  playing.style.height = "300px";
+  size = 100;
+  
+
+  let coordinates = {
+    top: 0,
+    left: 0,
+    value: 0,
+  };
+  let arr = [];
+  arr.push(coordinates);
+  function move(index) {
+    let number = arr[index];
+    let leftDiv = Math.abs(coordinates.left - number.left);
+    let topDiv = Math.abs(coordinates.top - number.top);
+    //если разница координат между ячейками 1, то это
+    //ячейка соседняя с пустой ячейкой
+    if (leftDiv + topDiv !== 1) {
+      return;
+    }
+    number.element.style.left = `${coordinates.left * size}px`;
+    number.element.style.top = `${coordinates.top * size}px`;
+    //координатам ячейки(number.left) присваеваем координаты пустой ячейки(coordinates.left ),
+    //а пустой присваеваем координаты ячейки(взаимозаменяем)
+    let coordinatesLeft = coordinates.left;
+    let coordinatesTop = coordinates.top;
+    coordinates.left = number.left;
+    coordinates.top = number.top;
+    number.left = coordinatesLeft;
+    number.top = coordinatesTop;
+    //проверка на выигрыш
+    let isVictory = arr.every((number) => {
+      return number.value === number.top * 3 + number.left;
+    });
+    if (isVictory) {
+      alert("ВЫ ВЫИГРАЛИ");
+    }
+  }
+
+  function eight() {
+    const result = [];
+    for (let i = 1; i < 9; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  arr1 = eight();
+  arrSort = arr1.sort(() => Math.random() - 0.5);
+
+  console.log(arrSort);
+  //for (let i = 0; i < arrSort.length; i++) {
+  for (let i = 1; i < 9; i++) {
+    const number = document.createElement("div");
+    let value = arrSort[i - 1];
+    number.className = "number";
+    number.innerHTML = value;
+    //задаём позиции для элементов
+    let left = i % 3; //номер колонки
+    let top = (i - left) / 3; //номер ряда
+    arr.push({
+      value: value,
+      left: left,
+      top: top,
+      element: number,
+    });
+    number.style.left = `${left * size}px`;
+    number.style.top = `${top * size}px`;
+    playing.append(number);
+    number.addEventListener("click", () => {
+      move(i);
+    });
+  }
+  /*for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    element.style.width = "50px";
+    element.style.height = "50px";
+    element.style.fontSize = "20px";
+  }
+  */
+}
+
+const size3 = document.querySelector(".size3");
+size3.addEventListener("click", threeRows);
+/////////////  6x6  ///////////////
+function sixRows() {
+  playing.innerHTML = "";
+  playing.style.width = "300px";
+  playing.style.height = "300px";
+  size = 50;
+  
+
+  let coordinates = {
+    top: 0,
+    left: 0,
+    value: 0,
+  };
+  let arr = [];
+  arr.push(coordinates);
+  function move(index) {
+    let number = arr[index];
+    let leftDiv = Math.abs(coordinates.left - number.left);
+    let topDiv = Math.abs(coordinates.top - number.top);
+    //если разница координат между ячейками 1, то это
+    //ячейка соседняя с пустой ячейкой
+    if (leftDiv + topDiv !== 1) {
+      return;
+    }
+    number.element.style.left = `${coordinates.left * size}px`;
+    number.element.style.top = `${coordinates.top * size}px`;
+    //координатам ячейки(number.left) присваеваем координаты пустой ячейки(coordinates.left ),
+    //а пустой присваеваем координаты ячейки(взаимозаменяем)
+    let coordinatesLeft = coordinates.left;
+    let coordinatesTop = coordinates.top;
+    coordinates.left = number.left;
+    coordinates.top = number.top;
+    number.left = coordinatesLeft;
+    number.top = coordinatesTop;
+    //проверка на выигрыш
+    let isVictory = arr.every((number) => {
+      return number.value === number.top * 6 + number.left;
+    });
+    if (isVictory) {
+      alert("ВЫ ВЫИГРАЛИ");
+    }
+  }
+
+  function eight() {
+    const result = [];
+    for (let i = 1; i < 36; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  arr1 = eight();
+  arrSort = arr1.sort(() => Math.random() - 0.5);
+
+  console.log(arrSort);
+  
+  for (let i = 1; i < 36; i++) {
+    const number = document.createElement("div");
+    let value = arrSort[i - 1];
+    number.className = "number";
+    number.innerHTML = value;
+    //задаём позиции для элементов
+    let left = i % 6; //номер колонки
+    let top = (i - left) / 6; //номер ряда
+    arr.push({
+      value: value,
+      left: left,
+      top: top,
+      element: number,
+    });
+    number.style.left = `${left * size}px`;
+    number.style.top = `${top * size}px`;
+    playing.append(number);
+    number.addEventListener("click", () => {
+      move(i);
+    });
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    element.style.width = "50px";
+    element.style.height = "50px";
+    element.style.fontSize = "20px";
+  }
+}
+const size6 = document.querySelector(".size6");
+size6.addEventListener("click", sixRows);
+///////////  7x7  //////////////////////
+
+function sevenRows() {
+  playing.innerHTML = "";
+  playing.style.width = "350px";
+  playing.style.height = "350px";
+  size = 50;
+  
+
+  let coordinates = {
+    top: 0,
+    left: 0,
+    value: 0,
+  };
+  let arr = [];
+  arr.push(coordinates);
+  function move(index) {
+    let number = arr[index];
+    let leftDiv = Math.abs(coordinates.left - number.left);
+    let topDiv = Math.abs(coordinates.top - number.top);
+    //если разница координат между ячейками 1, то это
+    //ячейка соседняя с пустой ячейкой
+    if (leftDiv + topDiv !== 1) {
+      return;
+    }
+    number.element.style.left = `${coordinates.left * size}px`;
+    number.element.style.top = `${coordinates.top * size}px`;
+    //координатам ячейки(number.left) присваеваем координаты пустой ячейки(coordinates.left ),
+    //а пустой присваеваем координаты ячейки(взаимозаменяем)
+    let coordinatesLeft = coordinates.left;
+    let coordinatesTop = coordinates.top;
+    coordinates.left = number.left;
+    coordinates.top = number.top;
+    number.left = coordinatesLeft;
+    number.top = coordinatesTop;
+    //проверка на выигрыш
+    let isVictory = arr.every((number) => {
+      return number.value === number.top * 7 + number.left;
+    });
+    if (isVictory) {
+      alert("ВЫ ВЫИГРАЛИ");
+    }
+  }
+
+  function eight() {
+    const result = [];
+    for (let i = 1; i < 49; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  arr1 = eight();
+  arrSort = arr1.sort(() => Math.random() - 0.5);
+
+  console.log(arrSort);
+  
+  for (let i = 1; i < 49; i++) {
+    const number = document.createElement("div");
+    let value = arrSort[i - 1];
+    number.className = "number";
+    number.innerHTML = value;
+    //задаём позиции для элементов
+    let left = i % 7; //номер колонки
+    let top = (i - left) / 7; //номер ряда
+    arr.push({
+      value: value,
+      left: left,
+      top: top,
+      element: number,
+    });
+    number.style.left = `${left * size}px`;
+    number.style.top = `${top * size}px`;
+    playing.append(number);
+    number.addEventListener("click", () => {
+      move(i);
+    });
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    element.style.width = "50px";
+    element.style.height = "50px";
+    element.style.fontSize = "20px";
+  }
+}
+
+const size7 = document.querySelector(".size7");
+size7.addEventListener("click", sevenRows);
+//////////////  5x5  //////////////////////////
+function fiveRows() {
+  playing.innerHTML = "";
+  playing.style.width = "350px";
+  playing.style.height = "350px";
+  size = 50;
+  
+
+  let coordinates = {
+    top: 0,
+    left: 0,
+    value: 0,
+  };
+  let arr = [];
+  arr.push(coordinates);
+  function move(index) {
+    let number = arr[index];
+    let leftDiv = Math.abs(coordinates.left - number.left);
+    let topDiv = Math.abs(coordinates.top - number.top);
+    //если разница координат между ячейками 1, то это
+    //ячейка соседняя с пустой ячейкой
+    if (leftDiv + topDiv !== 1) {
+      return;
+    }
+    number.element.style.left = `${coordinates.left * size}px`;
+    number.element.style.top = `${coordinates.top * size}px`;
+    //координатам ячейки(number.left) присваеваем координаты пустой ячейки(coordinates.left ),
+    //а пустой присваеваем координаты ячейки(взаимозаменяем)
+    let coordinatesLeft = coordinates.left;
+    let coordinatesTop = coordinates.top;
+    coordinates.left = number.left;
+    coordinates.top = number.top;
+    number.left = coordinatesLeft;
+    number.top = coordinatesTop;
+    //проверка на выигрыш
+    let isVictory = arr.every((number) => {
+      return number.value === number.top * 7 + number.left;
+    });
+    if (isVictory) {
+      alert("ВЫ ВЫИГРАЛИ");
+    }
+  }
+
+  function eight() {
+    const result = [];
+    for (let i = 1; i < 49; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  arr1 = eight();
+  arrSort = arr1.sort(() => Math.random() - 0.5);
+
+  console.log(arrSort);
+
+  for (let i = 1; i < 49; i++) {
+    const number = document.createElement("div");
+    let value = arrSort[i - 1];
+    number.className = "number";
+    number.innerHTML = value;
+    //задаём позиции для элементов
+    let left = i % 7; //номер колонки
+    let top = (i - left) / 7; //номер ряда
+    arr.push({
+      value: value,
+      left: left,
+      top: top,
+      element: number,
+    });
+    number.style.left = `${left * size}px`;
+    number.style.top = `${top * size}px`;
+    playing.append(number);
+    number.addEventListener("click", () => {
+      move(i);
+    });
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    element.style.width = "50px";
+    element.style.height = "50px";
+    element.style.fontSize = "20px";
+  }
+  
+}
+
+
+const size5 = document.querySelector(".size5");
+size5.addEventListener("click", fiveRows);
